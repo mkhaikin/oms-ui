1a4d223d09c084d6b5c145ce8b320f29281f4978
yefimkh <michael.khaikin@gmail.com>
2021-09-17 09:38:44 -0400

    check interceptor code

--- src/http/index.ts
+++ src/http/index.ts
@@ -25,9 +25,9 @@ $api.interceptors.response.use((config) =>{ //in case of 401
     if(error.response.status ==401 && error.config && !error.config._isRetry){
         originalRequest._isRetry = true
         try{
-        const response = await axios.get<AuthResponse>(`${API_URL}/refresh`, {withCredentials: true})
-        localStorage.setItem('token', response.data.accessToken)
-        return $api.request(originalRequest)
+            const response = await axios.get<AuthResponse>(`${API_URL}/refresh`, {withCredentials: true})
+            localStorage.setItem('token', response.data.accessToken)
+            return $api.request(originalRequest)
         } catch(e){
             console.log("Not authorazed!")
         }
