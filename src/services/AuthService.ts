e37dd6f91ded3f9209d99b6f9e3c0b96e65ffb25
yefimkh <michael.khaikin@gmail.com>
2021-10-04 15:51:42 -0400

    old v

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -4,24 +4,15 @@ import axios from 'axios'
 import { AuthResponse } from '../types/AuthResponse'
 
 export default class AuthService {
-    static async login(email: string, password: string): Promise< string | AxiosResponse<AuthResponse>>{
+    static async login(email: string, password: string): Promise< AxiosResponse<AuthResponse>>{
        
-        /*
-        const res = await axios.get("https://bins-collection-mysql.herokuapp.com/api/test/staff/users")
-        const data = res.data;
-        console.log("Response: " + res.data);
-        */
         console.log("login start post....");
-        //const postresp = await $api.post<AuthResponse>('/login', {email, password})
-        await $api.post<AuthResponse>('/login', {email, password}).then(postresp=>{
+        const postresp = await $api.post<AuthResponse>('/login', {email, password})
+        
 
         console.log("postresp: " + postresp.data.user.userbadge._positionid);
         return postresp
-        }).catch(error => {
-            console.log(error.response.data.error)
-            return error.response.data.error
-          })
-        return "Login error!..."  
+       
        //post(‘url’, {timeout: 3000})
         
     }
