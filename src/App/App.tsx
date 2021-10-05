a3e13716b10860db362f137712f57e66fa5d7511
yefimkh <michael.khaikin@gmail.com>
2021-10-05 11:27:09 -0400

    testing reducer and useTypesSelector

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -30,7 +30,8 @@ function App() {
 
   const {access, loading, error } = useTypesSelector(state=> state.login)
   //access?.user.userbadge._positionid
-
+  console.log("access: " + access?.user.userbadge._positionid)
+  
   useEffect( () => {
     (async function(){
       
@@ -66,7 +67,6 @@ function App() {
 //      </div>
 //    )
 //  } 
-
 
 
   return (
--- src/store/action-creators/accesscall.ts
+++ src/store/action-creators/accesscall.ts
@@ -9,7 +9,6 @@ export const login = (email: string, password: string) => {
             dispatch( {type: AccessActionTypes.FETCH_ACCESS})
             const response = await AuthService.login(email, password)
 
-console.log("accesscal responce: " + response.data.user.userbadge._positionid)
             dispatch( {type: AccessActionTypes.FETCH_ACCESS_SUCCESS, payload: response.data})
         } catch (e){
             dispatch( {type: AccessActionTypes.FETCH_ACCESS_ERROR, payload: "Error, " + e + " !"})
