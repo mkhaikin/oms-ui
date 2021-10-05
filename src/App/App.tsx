50a29233d650001582794667413c88edbc06375b
yefimkh <michael.khaikin@gmail.com>
2021-10-05 16:24:26 -0400

    id to num

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -31,7 +31,8 @@ function App() {
   const {access, loading, error } = useTypesSelector(state=> state.login)
   //access?.user.userbadge._positionid
   console.log("access in main, user id: " + access?.user.userbadge._id)
-  console.log("1 ???? access in main, user id: " + access?.user.userbadge._id.length)
+  console.log("access in main, user id as number: " + Number(access?.user.userbadge._id))
+  console.log("1 ???? access in main, user id length: " + access?.user.userbadge._id.length)
 
   useEffect( () => {
     (async function(){
@@ -50,7 +51,7 @@ function App() {
 */
   //let acc: boolean = false;
   //if(!userstore.isAuth){
-    console.log("2 ???? access in main, user id: " + access?.user.userbadge._id.length)
+    console.log("2 ???? access in main, user id length: " + access?.user.userbadge._id.length)
     if(access !== null && access.user.userbadge._id.length > 0){
       console.log("!!!! access in main, user id: " + access?.user.userbadge._id)
       return(
