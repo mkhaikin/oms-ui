1699b3bbc5dbdb987ade3c28eec2e89bc42c4de9
yefimkh <michael.khaikin@gmail.com>
2021-10-05 20:14:45 -0400

    useStyles

--- src/components/MainWork.js
+++ src/components/MainWork.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { ConfirmProvider } from "material-ui-confirm";
 
 import '../App/App.css';
-import { CssBaseline } from '@material-ui/core';
+import { CssBaseline, makeStyles } from '@material-ui/core';
 import Header from './Header';
 import Footer from './Footer';
 import CRMContainer from './CRMContainer';
 
 const MainWork = ()=>{
+    const classes = useStyles(); 
+    
     return (
     <ConfirmProvider>
     <React.Fragment>
